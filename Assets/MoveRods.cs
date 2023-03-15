1d7530428feadd67aa4be2de20a9afd92310be79
quipthaw <98988760+quipthaw@users.noreply.github.com>
2023-03-14 20:57:12 -0400

    Added Temp Chart and Rods, working on trigs/logic

---
+++ Assets/MoveRods.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRods : MonoBehaviour {
+    public Transform target;
+    public float speed;
+ 
+    private void Update() {
+        if (target != null) {
+            if (Vector3.Distance(transform.position, target.position) > speed * Time.deltaTime) {
+                transform.position += Time.deltaTime * speed * (target.transform.position - transform.position).normalized;
+            }
+            else {
+                transform.position = target.position;
+            }
+        }
+    }
+}
+
+
